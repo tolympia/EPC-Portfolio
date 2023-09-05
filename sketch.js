f778af0e26ea46ac8acf652725ef47977c9c3bf5
toylmpia <tozhu3@gmail.com>
2023-09-05 16:34:51 -0400

    Adds sketch files for gradient background

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 1000);
+}
+
+function createHorizontalGradient(color1, color2, leftX, rightX, upperY, lowerY){
+  var increment = 1.0 / (rightX - leftX);
+  for (var i = 0; i <= rightX - leftX; i++) {  
+    // Each vertical line is a slightly different color.
+    var lineColor = lerpColor(color1, color2, increment * i);
+    stroke(lineColor);
+    line(leftX + i, upperY, leftX + i, lowerY);
+  }
+}
+
+function draw() {
+  var color1 = color("#0E21A0");
+  var color2 = color("#EC53B0");
+  createHorizontalGradient(color1, color2, 0, width, 0, height);
+}
